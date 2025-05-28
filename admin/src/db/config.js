fa60751c2c6969e46f886652426773ee2bbe4567
MiguelAngel <miguelgiraldo1116@gmail.com>
2025-05-28 13:34:04 -0400

    ejecutando todo con docker

--- admin/src/db/config.js
+++ admin/src/db/config.js
@@ -1,7 +1,19 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: process.env.DATABASE_DIALECT,
-});
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//   dialect: process.env.DATABASE_DIALECT,
+// });
+
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.DATABASE_HOST,
+    port: 5432,
+    dialect: process.env.DATABASE_DIALECT,
+    logging: false,
+  }
+);
 
 export default sequelize;
